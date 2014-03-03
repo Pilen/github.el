bc8851136976334d6de9231723bdd73a5ea0082f
Pilen <fiskomaten@gmail.com>
2014-03-03 02:44:23 +0100

    added a function for listing your repos

--- github.el
+++ github.el
@@ -6,6 +6,7 @@
 (shell-command "echo hej")
 (get-buffer-create "*github-api*")
 (generate-new-buffer "*github-api*")
+(set-buffer (get-buffer-create "*github-api*"))
 (with-temp-buffer)
 
 (defun github-query (query)
@@ -28,5 +29,33 @@
 (dolist (name (github-repo-names)) (message name))
 
 
+(defun github-list-repos ()
+  (save-excursion
+    (set-buffer (get-buffer-create "*github*"))
+    (delete-region (point-min) (point-max))
+    (mapcar
+     (lambda (repo)
+       (let ((name (cdr (assoc 'name repo)))
+             (pushed_at (cdr (assoc 'pushed_at repo)))
+             (open_issues (cdr (assoc 'open_issues repo))))
+         (insert (format "%-20s" name) " "
+                 ;;name "\n"
+                 "last push: " pushed_at "    "
+
+                 (if (zerop open_issues)
+                     ""
+                   (concat (format "%6s " open_issues)
+                           (if (= 1 open_issues)
+                               "issue"
+                             "issues")))
+                 "\n\n")))
+     (github-query (concat "/users/" github-user-name "/repos?sort=updated")))))
+(github-list-repos)
 
 (start-process)
+
+
+oversigt:
+name
+open_issues
+pushed_at
